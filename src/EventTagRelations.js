21331a00a9850659935aa62b75cba1f4c1ae241b
krdevel_macmini_monterey <kristianbredin@gmail.com>
2022-04-30 12:30:40 +0200

    let eventMaxIndex = store.state.events.length

--- src/EventTagRelations.js
+++ src/EventTagRelations.js
@@ -3,8 +3,10 @@ import store from './store'
 const assignTagsToEvents = () => {
 	// console.log('assignTagsToEvents' + this.$store.state.events)
 	console.log('assignTagsToEvents' + store.state.events)
+	// Hardcoded quasi random assignment indexes
+	let eventMaxIndex = store.state.events.length
+	let tagMaxIndex = store.state.tags.length
+	console.log('' + eventMaxIndex + ' ' + tagMaxIndex)
 }
-
-// assignTagsToEvents()
 
 export default { assignTagsToEvents }
