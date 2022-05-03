d33e7aa38e7126f555c4d3bbb8bced0793dc96ef
ifyousayso <gplay@foshin.com>
2022-05-03 13:47:39 +0200

    Resolve validation errors

--- src/components/SearchResultList.vue
+++ src/components/SearchResultList.vue
@@ -8,17 +8,23 @@
 					this.events = events
 				})
 */
+			this.filterByTags([])
 		},
 		data() {
 			return {
 				//				events: null
+			}
+		},
+		methods: {
+			filterByTags(tags) {
+				console.log(tags)
 			}
 		}
 	}
 </script>
 
 <template>
-	<ul v-if="events !== null">
+	<ul v-if="$store.state.events !== null">
 		<li :key="event.id" v-for="event in $store.state.events">
 			{{ event.name }}
 		</li>
