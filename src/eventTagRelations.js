0a5b40aa0912850b1f334b73bf6485a6f48bd994
krdevel_macmini_monterey <kristianbredin@gmail.com>
2022-05-04 16:51:45 +0200

    Vidare

--- src/eventTagRelations.js
+++ src/eventTagRelations.js
@@ -12,40 +12,40 @@ const assignTagsToEvent = function (eventIndex, tagIndex) {
 
 const assignTagsToEvents = function () {
 	console.log('JavaScript: assignTagsToEvents ____')
-	// assignTagsToEvent(0, 0)
-	// assignTagsToEvent(0, 2)
-	// assignTagsToEvent(0, 5)
-	// assignTagsToEvent(0, 13)
+	assignTagsToEvent(0, 0)
+	assignTagsToEvent(0, 2)
+	assignTagsToEvent(0, 5)
+	assignTagsToEvent(0, 13)
 
-	// assignTagsToEvent(1, 1)
-	// assignTagsToEvent(1, 3)
-	// assignTagsToEvent(1, 5)
-	// assignTagsToEvent(1, 9)
+	assignTagsToEvent(1, 1)
+	assignTagsToEvent(1, 3)
+	assignTagsToEvent(1, 5)
+	assignTagsToEvent(1, 9)
 
-	// assignTagsToEvent(2, 1)
-	// assignTagsToEvent(2, 4)
-	// assignTagsToEvent(2, 7)
-	// assignTagsToEvent(2, 12)
+	assignTagsToEvent(2, 1)
+	assignTagsToEvent(2, 4)
+	assignTagsToEvent(2, 7)
+	assignTagsToEvent(2, 12)
 
 	// // Print out Events with tags in the console:
-	// printEventsWithTagsToConsole()
+	printEventsWithTagsToConsole()
 }
 
-// const printEventsWithTagsToConsole = function () {
-// 	let events = this.$store.state.events
-// 	for (let i = 0; i < events.length; i++) {
-// 		console.log('Event:')
-// 		console.log(' Name: ' + events[i].name)
-// 		console.log(' Tags:')
+const printEventsWithTagsToConsole = function () {
+	let events = this.$store.state.events
+	for (let i = 0; i < events.length; i++) {
+		console.log('Event:')
+		console.log(' Name: ' + events[i].name)
+		console.log(' Tags:')
 
-// 		let tags = events[i].tags
+		let tags = events[i].tags
 
-// 		for (let j = 0; j < tags.length; j++) {
-// 			let tag = this.getTagFromId(tags[j])
-// 			console.log('   ' + tag.type + ': ' + tag.text)
-// 		}
-// 	}
-// }
+		for (let j = 0; j < tags.length; j++) {
+			let tag = this.getTagFromId(tags[j])
+			console.log('   ' + tag.type + ': ' + tag.text)
+		}
+	}
+}
 
 export default assignTagsToEvents
 
