3335c054bc169ef6e3f2f9e3ac1b197bcca84cf1
krdevel_macmini_monterey <kristianbredin@gmail.com>
2022-05-04 16:16:58 +0200

    eventTagRelations.assignTagsToEvent(0, 0)

--- src/eventTagRelations.js
+++ src/eventTagRelations.js
@@ -1,1 +1,13 @@
 import store from './store'
+
+// this.$store.commit('addTagToEvent', {
+
+
+
+assignTagsToEvent(eventIndex, tagIndex) {
+    console.log('JavaScript: assignTagsToEvents')
+    this.store.commit('addTagToEvent', {
+        eventIndex: eventIndex,
+        tagIndex: tagIndex
+    })
+}
--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import eventTagRelations from './eventTagRelations'
 
 createApp(App).use(router).use(store).mount('#app')
+
+eventTagRelations.assignTagsToEvent(0, 0)
