40c935bcc4e0e8aba9239c8f3c4adb22120122e3
ifyousayso <gplay@foshin.com>
2022-05-05 11:18:23 +0200

    Add vue-router view structure

---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		data() {
+			return {
+				links: [
+					{ address: '/', title: 'home' },
+					{ address: '/filter', title: 'filter' }
+				]
+			}
+		}
+	}
+</script>
+
+<template>
+	<nav v-if="links.length !== 0">
+		<ul>
+			<li :key="link.address" v-for="link in links">
+				<RouterLink :to="link.address">{{ link.title }}</RouterLink>
+			</li>
+		</ul>
+	</nav>
+</template>
---
+++ src/views/FilterView.vue
@@ -0,0 +1,31 @@
+<script>
+	import SearchResultList from '../components/SearchResultList.vue'
+	import TagContainer from '../components/TagContainer.vue'
+
+	export default {
+		created() {},
+		//Programatisk Router exempel
+		/* ,
+		methods:{
+			tryClick(place){
+				this.$router.push('/event/'+place)
+			}
+		} */
+		components: {
+			SearchResultList,
+			TagContainer
+		}
+	}
+</script>
+
+<template>
+	<main>
+		<h1>Filter</h1>
+
+		<TagContainer type="event" />
+		<TagContainer type="location" />
+		<TagContainer type="date" />
+
+		<SearchResultList />
+	</main>
+</template>
