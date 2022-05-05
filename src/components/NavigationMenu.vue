4fb3b13a02c0cb0f84bff8fa63b3055329199121
Victor <victorgson@outlook.com>
2022-05-05 15:30:44 +0200

    updates navbar with bootstrap

--- src/components/NavigationMenu.vue
+++ src/components/NavigationMenu.vue
@@ -3,8 +3,8 @@
 		data() {
 			return {
 				links: [
-					{ address: '/', title: 'home' },
-					{ address: '/filter', title: 'filter' }
+					{ address: '/', title: 'Home' },
+					{ address: '/filter', title: 'Filter' }
 				]
 			}
 		}
@@ -12,11 +12,42 @@
 </script>
 
 <template>
-	<nav v-if="links.length !== 0">
+	<nav
+		v-if="links.length !== 0"
+		class="navbar navbar-expand-lg navbar-light bg-light"
+	>
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">
+				<img src="/favicon-196.png" alt="" width="30" height="24" />
+			</a>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon" />
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item" :key="link.address" v-for="link in links">
+						<RouterLink class="nav-link" :to="link.address">{{
+							link.title
+						}}</RouterLink>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<!-- <nav v-if="links.length !== 0">
 		<ul>
 			<li :key="link.address" v-for="link in links">
 				<RouterLink :to="link.address">{{ link.title }}</RouterLink>
 			</li>
 		</ul>
-	</nav>
+	</nav> -->
 </template>
