99e8364620df4f9afba8778b6935250d009c5675
krdevel <91143280+krdevel@users.noreply.github.com>
2022-05-07 12:41:18 +0200

    Kanske funkar på väg nu

--- src/eventTagRelations.js
+++ src/eventTagRelations.js
@@ -1,6 +1,6 @@
 import store from './store'
 
-let reAssignTagsToEvents = true
+let reAssignTagsToEvents = false
 
 let storeAreaForRead = store.state
 let storeAreaForWrite = store
@@ -29,7 +29,7 @@ function resetStateData() {
 
 const assignTagsToEvents = function () {
 	console.log('JavaScript: assignTagsToEvents ____')
-	printAssignedTags()
+	// printAssignedTags()
 
 	if (reAssignTagsToEvents) {
 		// resetStateData()
@@ -37,20 +37,20 @@ const assignTagsToEvents = function () {
 
 		storeAreaForWrite.commit('resetEventsTags')
 
-		// assignTagsToEvent(0, 0)
-		// assignTagsToEvent(0, 2)
-		// assignTagsToEvent(0, 5)
-		// assignTagsToEvent(0, 13)
+		assignTagsToEvent(0, 0)
+		assignTagsToEvent(0, 2)
+		assignTagsToEvent(0, 5)
+		assignTagsToEvent(0, 13)
 
-		// assignTagsToEvent(1, 1)
-		// assignTagsToEvent(1, 3)
-		// assignTagsToEvent(1, 5)
-		// assignTagsToEvent(1, 9)
+		assignTagsToEvent(1, 1)
+		assignTagsToEvent(1, 3)
+		assignTagsToEvent(1, 5)
+		assignTagsToEvent(1, 9)
 
-		// assignTagsToEvent(2, 1)
-		// assignTagsToEvent(2, 4)
-		// assignTagsToEvent(2, 7)
-		// assignTagsToEvent(2, 12)
+		assignTagsToEvent(2, 1)
+		assignTagsToEvent(2, 4)
+		assignTagsToEvent(2, 7)
+		assignTagsToEvent(2, 12)
 	}
 
 	// // Print out Events with tags in the console:
