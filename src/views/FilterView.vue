26d8257b825ea291a6c9606d6828803f2751e810
Victor <victorgson@outlook.com>
2022-05-10 14:05:33 +0200

    Added btn group for list/grid

--- src/views/FilterView.vue
+++ src/views/FilterView.vue
@@ -20,17 +20,54 @@
 
 <template>
 	<main id="filter-view">
-		<h1>Filter</h1>
-
 		<div>
 			<div>
 				<TagContainer type="event" />
 				<TagContainer type="location" />
 				<TagContainer type="date" />
 			</div>
-			<div>
+			<div id="list">
+				<div
+					class="btn-group"
+					role="group"
+					aria-label="Basic radio toggle button group"
+				>
+					<input
+						type="radio"
+						class="btn-check"
+						name="btnradio"
+						id="btnradio1"
+						autocomplete="off"
+						checked
+					/>
+					<label class="btn btn-outline-primary" for="btnradio1"
+						><i class="bi bi-list"></i
+					></label>
+
+					<input
+						type="radio"
+						class="btn-check"
+						name="btnradio"
+						id="btnradio2"
+						autocomplete="off"
+					/>
+					<label class="btn btn-outline-primary" for="btnradio2"
+						><i class="bi bi-grid"></i
+					></label>
+				</div>
 				<FilterList />
 			</div>
 		</div>
 	</main>
 </template>
+
+<style>
+	#list {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.btn-group {
+		align-self: flex-end;
+	}
+</style>
