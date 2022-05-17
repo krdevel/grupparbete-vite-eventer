6e3b23d033d171f0a3fbd49aba4d76591d0b479e
ifyousayso <gplay@foshin.com>
2022-05-17 11:49:04 +0200

    Adjust FilterView box paddings

--- src/views/FilterView.vue
+++ src/views/FilterView.vue
@@ -98,14 +98,12 @@
 
 	#filter-view .tag-container-box {
 		width: 35%;
+		padding: 2rem 1rem 2rem 2rem;
 	}
 
 	#filter-view .list-box {
 		width: 50%;
-	}
-
-	#filter-view main > div {
-		padding: 2rem;
+		padding: 2rem 2rem 2rem 1rem;
 	}
 
 	#filter-view .list-box {
@@ -147,6 +145,7 @@
 		#filter-view main > .tag-container-box,
 		#filter-view main > .list-box {
 			width: 100%;
+			padding: 1rem;
 		}
 	}
 </style>
