ef912cf534d79d30ca14e1f0ebf4e496de4c738b
Victor <victorgson@outlook.com>
2022-05-18 22:21:36 +0200

    adds prototype for create event page

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,20 +1,89 @@
 <template>
 	<div>
-		<form class="list">
-			<label> Event Name</label>
-			<input v-model="eventName" type="text" placeholder="Gabbes änglar" />
-			<label> Event Type</label>
-			<input v-model="typeTag" type="text" placeholder="Musik" />
-			<label> Event Location</label>
-			<input v-model="locationTag" type="text" placeholder="Stockholm" />
-			<label> Date</label>
-			<input v-model="dateTag" type="text" placeholder="2022-06-16" />
-			<label> Time</label>
-			<input v-model="timeTag" type="text" placeholder="20:00" />
-			<label> Description </label>
-			<textarea v-model="description"> </textarea>
-			<button type="submit" @click="submit(eventName)">Create</button>
-		</form>
+		<div class="headerImg">
+			<img class="imgData" :src="imgUri" />
+			<div class="imgText">
+				<h1
+					class="nameData"
+					name="eventName"
+					contenteditable="true"
+					@input="onInput"
+				>
+					{{ eventName }}
+				</h1>
+			</div>
+		</div>
+		<div class="container">
+			<div id="btnTags">
+				<input
+					list="typeTags"
+					class="btn-primary btnTest"
+					contenteditable="true"
+					@input="handleDataList"
+				/>
+				<datalist id="typeTags">
+					<option
+						v-for="(tag, id) in dbTypeTags"
+						:key="id"
+						:value="tag.text"
+					></option>
+				</datalist>
+
+				<input
+					list="locationTags"
+					class="btn-primary btnTest"
+					contenteditable="true"
+					@input="handleDataList"
+				/>
+				<datalist id="locationTags">
+					<option
+						v-for="(tag, id) in dbLocationTags"
+						:key="id"
+						:value="tag.text"
+					></option>
+				</datalist>
+
+				<input
+					list="dateTags"
+					class="btn-primary btnTest"
+					contenteditable="true"
+					@input="handleDataList"
+				/>
+				<datalist id="dateTags">
+					<option
+						v-for="(tag, id) in dbDateTags"
+						:key="id"
+						:value="tag.text"
+					></option>
+				</datalist>
+
+				<input
+					list="timeTags"
+					class="btn-primary btnTest"
+					contenteditable="true"
+					@input="handleDataList"
+				/>
+				<datalist id="timeTags">
+					<option
+						v-for="(tag, id) in dbTimeTags"
+						:key="id"
+						:value="tag.text"
+					></option>
+				</datalist>
+			</div>
+			<p
+				class="descText"
+				contenteditable="true"
+				name="description"
+				@input="onInput"
+			>
+				{{ description }}
+			</p>
+		</div>
+
+		<button class="btn btn-outline-primary center" @click="submit">
+			Create!
+		</button>
 	</div>
 </template>
 
@@ -24,38 +93,64 @@
 	export default {
 		data() {
 			return {
+				// default values / placeholders
 				eventName: 'Gabbes Änglar',
 				typeTag: 'Musik',
 				locationTag: 'Göteborg',
 				dateTag: '2022-06-18',
 				timeTag: '18:00',
-				description: '',
+				description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+				doloribus a dolores cupiditate facere? Magni dolorum animi repudiandae,
+				iusto est mollitia tenetur veniam nisi vel, eligendi reiciendis ullam,
+				sed ad!`,
+				imgUri:
+					'https://kopakrypto.se/wp-content/uploads/sites/6/2021/12/olika-kryptovalutor.jpg',
+
 				event: {},
-				imgUri: 'www.google.com'
+				// all tags that are retrieved on created()
+
+				dbTypeTags: null,
+				dbLocationTags: null,
+				dbDateTags: null,
+				dbTimeTags: null
 			}
 		},
-		computed: {
-			nameOfEvent() {
-				return this.eventName
-			}
+		created() {
+			this.dbTypeTags = this.$store.state.dbTypeTags
+			this.dbLocationTags = this.$store.state.dbLocationTags
+			this.dbDateTags = this.$store.state.dbDateTags
+			this.dbTimeTags = this.$store.state.dbTimeTags
 		},
 		methods: {
+			// handles input from "buttons", not working properly with v-model
+			handleDataList(e) {
+				const variableToUpdate = e.target.getAttribute('name')
+				this[variableToUpdate] = e.target.value
+			},
+			// handles input from html elemnts tagged with editable, not working with v-model
+			onInput(e) {
+				const variableToUpdate = e.target.getAttribute('name')
+				this[variableToUpdate] = e.target.innerText
+			},
+
 			submit() {
 				this.event = {
 					id: 'a33abc11-264e-4bbb-82e8-b87226bb4383',
 					name: this.eventName,
-					type: null,
-					location: null,
-					date: null,
-					time: null,
+					type: this.typeTag,
+					location: this.locationTag,
+					date: this.dateTag,
+					time: this.timeTag,
 					image:
 						'https://kopakrypto.se/wp-content/uploads/sites/6/2021/12/olika-kryptovalutor.jpg',
-					description: null,
+					description: this.description,
 					likes: 0,
 					likeBool: false
 				}
 
+				// adds event to db
 				this.$store.commit('createEvent', this.event)
+				// assigns tags in db
 				this.$store.commit('dbSetEventTags', {
 					eventName: this.eventName,
 					typeTagText: this.typeTag,
@@ -63,6 +158,9 @@
 					dateTagText: this.dateTag,
 					timeTagText: this.timeTag
 				})
+
+				// navigates to the created event
+				this.$router.push({ path: `/event/${this.event.id}` })
 			}
 		}
 	}
@@ -74,5 +172,66 @@
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
+	}
+
+	#btnTags {
+		display: flex;
+		justify-content: center;
+	}
+	.btnTest {
+		background-color: #1f3868;
+		color: #f1f6ff;
+		border: 0;
+		border-radius: 1.5rem;
+		padding: 0.75rem;
+		width: 8rem;
+		margin: 5px;
+	}
+	#likeIcon {
+		display: flex;
+		justify-content: center;
+	}
+	.nameData {
+		color: black;
+	}
+
+	.headerImg {
+		position: relative;
+	}
+
+	.imgText {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+	.imgData {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+		max-width: 100%;
+		height: auto;
+		border-radius: 8px;
+		object-fit: contain;
+		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+		opacity: 0.7;
+	}
+	#iconTwo {
+		color: red;
+	}
+	#iconTwo:hover {
+		color: black;
+	}
+	#iconOne {
+		color: red;
+	}
+	.descText {
+		text-align: center;
+	}
+
+	.center {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
 	}
 </style>
