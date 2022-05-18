1a2ae6f2581ddd3f7bd4f19c6ef3b76ab974e702
krdevel_macmini_monterey <kristianbredin@gmail.com>
2022-05-18 19:49:12 +0200

    .btn in bootstrap.scss

--- src/components/TagComponent.vue
+++ src/components/TagComponent.vue
@@ -52,27 +52,27 @@
 </script>
 
 <style lang="scss" scoped>
-	.btn {
-		/* This overrides Bootstrap */
-		background-color: #f1f6ff;
-		color: #1f3868;
-		border: 0;
-		border-radius: 1.5rem;
-		padding: 0.75rem;
-		width: 8rem;
+	// .btn {
+	// 	/* This overrides Bootstrap */
+	// 	background-color: #f1f6ff;
+	// 	color: #1f3868;
+	// 	border: 0;
+	// 	border-radius: 1.5rem;
+	// 	padding: 0.75rem;
+	// 	width: 8rem;
 
-		margin: 0.5rem;
-		$badge-color: black;
-	}
+	// 	margin: 0.5rem;
+	// 	$badge-color: black;
+	// }
 
-	/* This overrides Bootstrap */
-	.btn:focus {
-		box-shadow: none;
-	}
+	// /* This overrides Bootstrap */
+	// .btn:focus {
+	// 	box-shadow: none;
+	// }
 
-	/* This overrides Bootstrap */
-	.btn.btn-primary {
-		background-color: #1f3868;
-		color: #f1f6ff;
-	}
+	// /* This overrides Bootstrap */
+	// .btn.btn-primary {
+	// 	background-color: #1f3868;
+	// 	color: #f1f6ff;
+	// }
 </style>
