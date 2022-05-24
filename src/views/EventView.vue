2e36468a12191f2d53f70b6e69160c1755696ffb
Vincent <vincent.pilstrom.tipmung@iths.se>
2022-05-25 01:14:22 +0200

    EventView style and some fixes made!

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,41 +1,37 @@
 <template>
-	<main class="container">
+	<main>
 		<div class="container">
 			<h1>Event > {{ event.location }}</h1>
 		</div>
 		<div class="headerImg">
 			<img class="imgData" :src="event.image" />
-
-			<!-- <div class="imgText">
-				<h1 class="nameData">{{ event.name }}</h1>
-			</div> -->
 		</div>
 		<div class="container">
 			<div id="likeIcon">
-				<div v-if="likeCheck">
-					<h1>
-						<i
-							id="iconOne"
-							class="bi bi-heart-fill"
-							type="button"
-							@click="likeBtnF"
-						></i>
-					</h1>
+				<h1>{{ event.name }}</h1>
+				<div v-if="likeCheck" class="likeCount">
+					<i
+						id="iconOne"
+						class="bi bi-heart-fill"
+						type="button"
+						@click="likeBtnF"
+					></i>
+
+					<p class="likedCount">{{ likedEvent }}</p>
 				</div>
-				<div v-else>
-					<h1>
-						<i
-							id="iconTwo"
-							class="bi bi-heart"
-							type="button"
-							@click="likeBtnT"
-						></i>
-					</h1>
+				<div v-else class="likeCount">
+					<i
+						id="iconTwo"
+						class="bi bi-heart"
+						type="button"
+						@click="likeBtnT"
+					></i>
+
+					<p class="likedCount">{{ likedEvent }}</p>
 				</div>
 			</div>
-			<p id="likeIcon">{{ likedEvent }}</p>
-			<h1>{{ event.name }}</h1>
-			<div id="btnTags">
+
+			<div class="btnTags">
 				<button class="btn btn-primary" disabled>
 					{{ event.type }}
 				</button>
@@ -51,39 +47,31 @@
 			</div>
 			<p class="descText">{{ event.description }}</p>
 			<div v-if="shareBool" class="shareBox">
-				<h1>
-					<i class="bi bi-share-fill" type="button" @click="setShareFalse"></i>
-				</h1>
+				<i class="bi bi-share-fill" type="button" @click="setShareFalse"></i>
 
-				<h1 id="linkOne">
-					<a :href="shareFb"><i class="bi bi-facebook" type="button"></i></a>
-				</h1>
-				<h1 id="linkTwo">
-					<a :href="shareTwitter">
-						<i class="bi bi-twitter" type="button" @click="shareTwitter"></i
-					></a>
-				</h1>
-				<div id="linkThree">
-					<input class="linkText" :value="share" readonly />
-					<button
-						id="linkBtn"
-						class="btn btn-outline-primary"
-						aria-pressed="true"
-						@click="copyText(linkEvent())"
-					>
-						Copy Link <i class="bi bi-clipboard2"></i>
-					</button>
-				</div>
+				<a :href="shareFb"><i class="bi bi-facebook" type="button"></i></a>
+
+				<a :href="shareTwitter">
+					<i class="bi bi-twitter" type="button" @click="shareTwitter"></i
+				></a>
+
+				<input class="linkText" :value="share" readonly />
+				<button
+					id="linkBtn"
+					class="btn btn-outline-primary"
+					aria-pressed="true"
+					@click="copyText(linkEvent())"
+				>
+					Copy Link <i id="iconBtnSize" class="bi bi-clipboard2"></i>
+				</button>
 			</div>
 			<div v-else class="shareBox">
-				<h1 id="ikonF">
-					<i
-						id="shareIkonF"
-						class="bi bi-share"
-						type="button"
-						@click="setShareTrue"
-					></i>
-				</h1>
+				<i
+					id="shareIkonF"
+					class="bi bi-share"
+					type="button"
+					@click="setShareTrue"
+				></i>
 			</div>
 		</div>
 	</main>
@@ -161,57 +149,48 @@
 </script>
 
 <style lang="scss" scoped>
-	#btnTags {
-		width: auto;
-		margin-top: 3rem;
-		text-align: left;
+	.container {
+		max-width: 90% !important;
 	}
-	#likeIcon {
-		display: flex;
-		justify-content: right;
-	}
-	.nameData {
-		color: black;
-		text-align: center;
-	}
-
 	.headerImg {
 		position: relative;
 	}
-
-	.imgText {
-		// 		margin: 30px;
-		// 		background-color: #ffffff;
-		// opacity: 0.6;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-	}
 	.imgData {
 		display: block;
-		//width: 64rem;
-		//height: 36.8125rem;
 		width: 100%;
 		max-height: 36rem;
 		min-height: 10rem;
 		object-fit: cover;
 		margin: auto;
-
-		//opacity: 0.7;
 	}
+	#likeIcon {
+		margin-top: 2rem;
+		display: flex;
+		justify-content: space-between;
+	}
+
 	#iconTwo {
 		color: red;
-
-		//background: rgba(242, 78, 30, 0.15);
 	}
 	#iconTwo:hover {
 		color: red;
-		text-shadow: 0 0 11px red;
+		text-shadow: 0 0 0.5rem red;
 	}
 	#iconOne {
 		color: red;
 	}
+	.likeCount {
+		display: table-row;
+		text-align: center;
+	}
+	.likeCount > i {
+		font-size: 2.5rem;
+	}
+	.btnTags {
+		width: auto;
+		text-align: left;
+	}
+
 	.descText {
 		margin-top: 2rem;
 		text-align: left;
@@ -220,33 +199,36 @@
 	}
 	.shareBox {
 		display: flex;
-		align-items: baseline;
+		width: auto;
+		align-items: center;
 		margin-bottom: 5rem;
 		margin-top: 2rem;
 	}
+	.shareBox > i,
+	.shareBox > a {
+		font-size: 2.5rem;
+	}
+
+	.shareBox > *:not(:last-child) {
+		margin-right: 1.5rem;
+	}
 	#shareIkonF:hover {
-		//color: darkblue;
-		//box-shadow: ;
-		text-shadow: 0 0 11px black;
+		text-shadow: 0 0 0.5rem black;
 	}
-	#linkOne {
-		margin-right: 0.5rem;
-		margin-left: 1rem;
-	}
-	#linkTwo {
-		margin-right: 1rem;
-	}
-	#linkThree {
-		margin-bottom: -3rem;
-	}
+
 	.linkText {
+		width: 47%;
 		border-radius: 10px;
 		color: black;
 		border-color: #fae8f5;
 	}
+
 	#linkBtn {
-		margin-bottom: 0px;
-		margin-top: 0px;
-		margin-right: 0px;
+		margin: 0;
+		display: flex;
+		align-items: center;
+	}
+	#iconBtnSize {
+		font-size: 1.5rem;
 	}
 </style>
