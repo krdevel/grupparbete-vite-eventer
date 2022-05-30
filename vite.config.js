b4fbe4533214d360bfce0ce02bf2aff54113c4c6
krdevel_macmini_monterey <kristianbredin@gmail.com>
2022-05-30 17:38:00 +0200

    defineConfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,47 @@ import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-	plugins: [vue()],
-	server: { host: true }
+	plugins: [
+		VitePWA({
+			manifest: {
+				background_color: 'grey',
+				display: 'standalone',
+				icons: [
+					{
+						purpose: 'any',
+						sizes: '192x192',
+						src: 'manifest-icon-192.maskable.png',
+						type: 'image/png'
+					},
+					{
+						purpose: 'maskable',
+						sizes: '192x192',
+						src: 'manifest-icon-192.maskable.png',
+						type: 'image/png'
+					},
+					{
+						purpose: 'any',
+						sizes: '512x512',
+						src: 'manifest-icon-512.maskable.png',
+						type: 'image/png'
+					},
+					{
+						purpose: 'maskable',
+						sizes: '512x512',
+						src: 'manifest-icon-512.maskable.png',
+						type: 'image/png'
+					}
+				],
+				name: 'Eventer',
+				short_name: 'Eventer',
+				start_url: '.',
+				theme_color: 'pink'
+			},
+			registerType: 'autoUpdate',
+			workbox: {
+				globPatterns: ['assets/**', 'index.html', 'manifest.webmanifest']
+			}
+		}),
+		vue()
+	]
 })
